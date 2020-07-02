52d38f0933bb8d1bbc5cc88dd4e3d2fe61cfbe77
Hewr Srood <hewr_s@outlook.com>
2020-07-02 19:34:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ function map(arr, callbackFunc) {
 }
 
 function reduce(arr, callbackFunc, startgPoint) {
-  let newValue = startgPoint ? startgPoint : arr[0];
-
+  let newValue = startgPoint ? startgPoint : arr[0]
   for (let i = startgPoint ? 0 : 1; i < arr.length; i++) {
-    newVal = callbackFunc(arr[i], newValue)
+    newValue = callbackFunc(arr[i], newValue)
   }
-  return newVal
+  return newValue
 }
